0c0ae2fc09471f2c39dc6159de5ce61b9bac9b7f
CHIDI J <git@gitlab.com:john.nwogu96/qm-chatbot.git>
2020-02-15 19:33:59 +0000

    Fix Log File

--- log.py
+++ log.py
@@ -1,27 +1,32 @@
 from cryptography.fernet import Fernet
 import json
+from os import path
 
 
 class Log:
     _chat_log = {}
-    _data_file = None
 
     @staticmethod
-    def _initialize_file():
-        try:
-            Log._data_file = open('log.txt', 'w')
-            Log._chat_log = json.load(Log._data_file)
-        except:
-            pass
+    def initialize_file():
+
+        if path.exists('log.txt'):
+            log_file = open('log.txt')
+            Log._chat_log = json.load(log_file)
+            log_file.close()
+        else:
+            with open('log.txt', 'w') as log_file:
+                json.dump({}, log_file)
+                log_file.close()
 
     @staticmethod
     def add(username, chat):
-
-        Log._initialize_file()
 
         if username in Log._chat_log.keys():
             Log._chat_log[username].append(chat)
         else:
             Log._chat_log[username] = [chat]
 
-        json.dump(Log._chat_log, Log._data_file, index=4)
+    @staticmethod
+    def close():
+        with open('log.txt', 'w') as log_file:
+            json.dump(Log._chat_log, log_file, indent=4)
